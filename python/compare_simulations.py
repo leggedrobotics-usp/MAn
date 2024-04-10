4279f77a144961ab7287d7371b9b6dd18b6e4281
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2024-04-10 09:53:33 -0300

    [ADDED] python graphics scripts and requirements.

--- python/compare_simulations.py
+++ python/compare_simulations.py
@@ -7,8 +7,12 @@ def main():
     parser = argparse.ArgumentParser(prog="Compare Simulations",
                                      description="Generate Time x [variables] plots to compare")
     parser.add_argument("-f", "--files", action="append")
+    parser.add_argument("-l", "--labels", action="append")
+    parser.add_argument("-t", "--title")
     args = parser.parse_args()
     print(args.files)
+    print(args.labels)
+    title = 'Time x Accumulated Interaction Forces' if args.title is None else args.title
     
     # Open first file to get inital columns
     big_table = pd.read_csv(args.files[0])
@@ -25,13 +29,17 @@ def main():
         big_table = pd.merge(big_table, table, left_on='time', right_on='time', how='left')
         # big_table = big_table.join(table.set_index('time'), on='time')
     
-    labels = big_table.columns[1:]
-    plt.figure(figsize=(21, 9), dpi=72)
-    plt.plot(big_table['time'].to_list(),np.array([big_table[c].to_list() for c in labels]).T)
+    col_labels = list(big_table.columns[1:])
+    labels = list(args.labels) + list(big_table.columns[(1 + len(args.labels)):])
+    plt.figure(figsize=(21/2, 9/2), dpi=150)
+    plt.plot(big_table['time'].to_list(),np.array([big_table[c].to_list() for c in col_labels]).T)
     plt.legend(labels)
-    plt.title(f'Time x Joint Positions')
+    # plt.title(f'Time x Joint Positions')
+    # plt.xlabel('Time (s)')
+    # plt.ylabel('Joint Positions (rad)')
+    plt.title(f'{title}')
     plt.xlabel('Time (s)')
-    plt.ylabel('Joint Positions (rad)')
+    plt.ylabel('Accumulated Interaction Forces (N)')
     plt.grid(True)
     plt.savefig("test.svg", format="svg")
     plt.show()
---
+++ python/compare_simulations_per_joint.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser(prog="Compare Simulations Per Joints",
+                                     description="Generate Time x [variables] plots to compare")
+    parser.add_argument("-f", "--files", action="append")
+    parser.add_argument("-l", "--labels", action="append")
+    parser.add_argument("-c", "--columns", action="append")
+    parser.add_argument("-y", "--ylabels", action="append")
+    parser.add_argument("-t", "--title")
+    args = parser.parse_args()
+    njnt = 2 # number of joints
+    print(args.files)
+    print(args.labels)
+    print(args.ylabels)
+    print(args.columns)
+    title = 'Time x Interaction Torques' if args.title is None else args.title
+    
+    # Open first file to get inital columns
+    input_tables = list()
+    out_tables = list()
+    time_table = pd.read_csv(args.files[0])[['time']]
+    
+    for file in args.files:
+        table = pd.read_csv(file)
+        input_tables.append(table)
+    
+    col_labels = list(args.columns)
+    labels = list(args.labels) + list(table.columns[(1 + len(args.labels)):])
+
+    for c in col_labels:
+        out_tables.append(time_table.copy())
+
+    for (o, c) in enumerate(col_labels):
+        for i in range(len(input_tables)):
+            in_table = input_tables[i][['time',c]]
+            out_table = out_tables[o]
+            out_tables[o] = pd.merge(out_table, in_table, left_on='time', right_on='time', how='left')
+    
+    fig, axs = plt.subplots(len(out_tables))
+    fig.suptitle(f'{title}')
+    fig.dpi = 150
+    fig.set_size_inches((21/2, 9/2))
+
+
+    for (ax, table, ylabel) in zip(axs, out_tables, args.ylabels):
+        tcolumns = table.columns[1:]
+        ys = np.array([table[c].to_list() for c in tcolumns]).T
+        x = table['time'].to_list()
+        mx = np.min(x)
+        Mx = np.max(x)
+        ax.plot(x,ys)
+        ax.grid(True)
+        ax.set_xlim([mx, Mx])
+        ax.set_ylabel(ylabel)
+        linspace = np.linspace(mx, Mx, 6).round().astype(np.int32).tolist()
+        print(f'linspace:{linspace}')
+        ax.set_xticks(linspace)
+
+    plt.legend(labels)
+    fig.supxlabel('Time (s)')
+    fig.supylabel('Interaction Torques (N.m)')
+
+    figname = f"{os.getcwd().split(os.path.sep)[-1]}"
+    print(f'figname:{figname}')
+    fig.savefig(f"{figname}.svg", format="svg")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
