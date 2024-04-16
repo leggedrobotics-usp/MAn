40ddd32e5cab118fc222de347d0801dc13298e4f
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2024-04-16 14:53:07 -0300

    [ADDED] compare simulations plot generator.

--- python/compare_simulations_per_joint.py
+++ python/compare_simulations_per_joint.py
@@ -11,6 +11,7 @@ def main():
     parser.add_argument("-l", "--labels", action="append")
     parser.add_argument("-c", "--columns", action="append")
     parser.add_argument("-y", "--ylabels", action="append")
+    parser.add_argument("-w", "--time_window", action="append")
     parser.add_argument("-t", "--title")
     args = parser.parse_args()
     njnt = 2 # number of joints
@@ -18,12 +19,16 @@ def main():
     print(args.labels)
     print(args.ylabels)
     print(args.columns)
+    print(args.time_window)
     title = 'Time x Interaction Torques' if args.title is None else args.title
     
     # Open first file to get inital columns
     input_tables = list()
     out_tables = list()
-    time_table = pd.read_csv(args.files[0])[['time']]
+    time_table = pd.read_csv(args.files[0])
+    if len(args.time_window) == 2:
+        time_table = time_table[(time_table['time'] >= float(args.time_window[0])) & (time_table['time'] <= float(args.time_window[1]))]
+        time_table = time_table[['time']]
     
     for file in args.files:
         table = pd.read_csv(file)
@@ -42,7 +47,9 @@ def main():
             out_tables[o] = pd.merge(out_table, in_table, left_on='time', right_on='time', how='left')
     
     fig, axs = plt.subplots(len(out_tables))
-    fig.suptitle(f'{title}')
+    fig.suptitle(f'{title}',fontsize=20)
+    fig
+    
     fig.dpi = 150
     fig.set_size_inches((21/2, 9/2))
 
@@ -54,16 +61,19 @@ def main():
         mx = np.min(x)
         Mx = np.max(x)
         ax.plot(x,ys)
+        ax.title.set_fontsize(20)
+        ax.xaxis.label.set_fontsize(20)
+        ax.yaxis.label.set_fontsize(20)
         ax.grid(True)
         ax.set_xlim([mx, Mx])
         ax.set_ylabel(ylabel)
-        linspace = np.linspace(mx, Mx, 6).round().astype(np.int32).tolist()
+        linspace = np.linspace(mx, Mx, 11).round().astype(np.int32).tolist()
         print(f'linspace:{linspace}')
         ax.set_xticks(linspace)
 
-    plt.legend(labels)
-    fig.supxlabel('Time (s)')
-    fig.supylabel('Interaction Torques (N.m)')
+    plt.legend(labels,fontsize=14)
+    fig.supxlabel('Time (s)',fontsize=20)
+    fig.supylabel('Interaction Torques (N.m)',fontsize=20)
 
     figname = f"{os.getcwd().split(os.path.sep)[-1]}"
     print(f'figname:{figname}')
